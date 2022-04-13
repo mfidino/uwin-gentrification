83d216798349ef48d164ef879c81c68b32978651
Mason Fidino <mfidino@lpzoo.org>
2022-04-13 15:08:59 -0500

    started compiling coordinates, new detection data

--- R/compile_coordinates.R
+++ R/compile_coordinates.R
@@ -3,38 +3,99 @@ library(dplyr)
 
 # some of the data is already ready to go.
 
-dat <- read.csv("../uwin-dataset/data_2021-07-27/cleaned_data/full_capture_history.csv")
+dat <- read.csv("../uwin-dataset/data_2022-04-11/cleaned_data/full_capture_history.csv")
+
+#
+dat$City <- gsub("phaz2", "phaz", dat$City)
+dat$City <- gsub("oaca|safa", "baca", dat$City)
+
+#dat$City <- gsub("phaz2", "phaz", dat$City)
+
+dat2 <- read.csv("./data/detection_data.csv")
+
+# reduce down to cities in dat2
+dat <- dat[dat$City %in% c(unique(dat2$City)),]
+
+# reduce down to correct seasons
+dat <- dat[dat$Season %in% unique(dat2$Season),]
+
+# and then species
+dat <- dat[dat$Species %in% unique(dat2$Species),]
+
+# change some city names and reorder
+dat$Season <- factor(dat$Season, levels = unique(dat$Season))
+
+# drop any sites with absolutely zero data
+zdat <- dat %>% 
+  dplyr::group_by(Site,City) %>% 
+  dplyr::summarise(ssamp = sum(J>0)) %>% 
+  dplyr::filter(ssamp == 0)
 
 
-dat <- dat[,c("City", "Site", "Long", "Lat", "Crs")]
-dat <- dat[!duplicated(dat),]
+dat <- dat[-which(dat$Site %in% zdat$Site),]
 
-cities <- c(
-  "ahga",
-  "autx",
-  "boma",
-  "buny",
-  "chil",
-  "deco",
-  "inin",
-  "ioio",
-  "jams",
-  "lrar",
-  "lbca",
-  "mawi",
-  "naca",
-  "phaz",
-  "phaz2",
-  "poor",
-  "rony",
-  "scut",
-  "sewa",
-  "slmo",
-  "tawa",
-  "toon",
-  "uril",
-  "wide"
+# drop autx
+dat <- dat[-which(dat$City == "autx"),]
+
+write.csv(dat, "./data/detection_data.csv", row.names = FALSE)
+
+# drop some season data here MASON
+
+# get number of sites sampled per city
+hm <- dat[dat$Species == dat$Species[1],] %>%
+  dplyr::group_by(Season, City) %>% 
+  dplyr::summarise(ssamp = sum(J>0))
+hm <- hm[order(hm$City, hm$Season),]
+
+hm <- split(
+  hm,
+  factor(hm$City)
 )
+# censor all of autx
+hm$autx$ssamp <- 0
+
+# turn all 10 or less to zero as well
+
+for(i in 1:length(hm)){
+  hm[[i]]$ssamp[hm[[i]]$ssamp <= 10] <- 0
+}
+
+write.csv(dat, "./data/detection_data.csv")
+
+sf <- dat[dat$City == "safa",]
+
+sf <- distinct(sf[,c("Site", "Long", "Lat")])
+
+tmp <- SELECT(
+  "select cl.locationAbbr, cl.utmEast, cl.utmNorth, cl.utmZone from CameraLocations cl
+  inner join StudyAreas sa on sa.areaID = cl.areaID
+  and sa.areaAbbr IN ('safa')"
+)
+
+library(sf)
+
+
+tmp <- sf::st_as_sf(
+  tmp,
+  coords = c("utmEast", "utmNorth"),
+  crs = 32610
+)
+
+my_df <- data.frame(
+  site = tmp$locationAbbr,
+  long = st_coordinates(t2)[,1],
+  lat = st_coordinates(t2)[,2],
+  crs = 4326,
+  city = "SAFA"
+)
+
+t2 <- sf::st_transform(
+  tmp,
+  crs = 4326
+)
+plot(st_coordinates(t2))
+
+aa <- season_availability(dat, "here.tiff", TRUE)
 
 dat <- dat %>% group_by(Site,City) %>% 
   summarise(Long = mean(Long), Lat = mean(Lat)) %>% 
